5ad33f35557b5eed951a9c48b2840550de91c644
yourfriend143 <ramukakayourdada143@gmail.com>
2025-08-06 21:14:06 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,8 +2,8 @@ import os
 from os import environ
 
 # API Configuration
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "23283708"))
+API_HASH = os.environ.get("API_HASH", "7805011fb84729023531f0fa3f000bec")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 CREDIT = os.environ.get("CREDIT", "〱ＵＧ▕")
@@ -59,3 +59,4 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
